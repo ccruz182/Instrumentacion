af2b043b7e600d8cdf9be2fc1b5c9eea392e29ed
Cesar Cruz <cesar.cruz182@gmail.com>
2018-11-13 08:04:18 -0600

    Versión de prueba de lectura de ADC0809 con Arduino y su envío vía BT

---
+++ Practica ADC BT/arduino/practica7.ino
@@ -0,0 +1,55 @@
+void setup() {
+  /* Pines de escritura (eleccion de sensor) */
+  pinMode(8, OUTPUT); // LSB
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT); // MSB
+
+  /* Pines de entrada */
+  pinMode(22, INPUT); // LSB
+  pinMode(24, INPUT);
+  pinMode(26, INPUT);
+  pinMode(28, INPUT);
+  pinMode(30, INPUT);
+  pinMode(32, INPUT);
+  pinMode(34, INPUT);
+  pinMode(36, INPUT); // MSB
+
+  Serial.begin(9600); // Inicio del Bluetooth
+}
+
+void loop() {
+  int valor1, valor2;
+  /* Sensor #1 */
+  seleccionSensor(LOW, LOW, LOW); // Seleccion del sensor
+  valor1 = lecturaBits(8); // Lectura del puerto
+  delay(500); // Se espera tiempo
+
+  /* Sensor #2 */
+  seleccionSensor(LOW, LOW, HIGH); // Seleccion del sensor
+  valor2 = lecturaBits(8); // Lectura del puerto
+  delay(500); // Se espera tiempo
+
+  /* Envio BT */
+  String valores = valor1 + "&" + valor2;
+  Serial.println(valores); // Se envía dato
+  Serial.flush(); // Se espera a que se envíe todo para poder continuar
+}
+
+int lecturaBits(int numBits) {
+  int i = 0;
+  int base;
+  int valor = 0;
+  for (i = 0, base = 22; i < numBits; i++, base += 2) {
+    if (digitalRead(base) == HIGH) {
+      valor += pow(2, i);
+    }
+  }
+
+  return valor;
+}
+
+void seleccionSensor(int a, int b, int c) {
+  digitalWrite(10, a);
+  digitalWrite(9, b);
+  digitalWrite(8, c);
+}
